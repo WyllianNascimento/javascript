e0338a188413ae42f091a199695657f930cc0fc5
Wyllian Nascimento <Wyllian.nascimento@gmail.com>
2022-01-21 15:13:31 -0300

    Update ex015.js

--- aula13/ex015.js
+++ aula13/ex015.js
@@ -2,7 +2,16 @@ var c = 1
 
 // Estrutura de repetição com teste lógico no início
 
+/*
 while (c <= 7) {
     console.log(`Passo ${c}`)
     c++
 }
+*/
+
+// Estrutura de repetição com teste lógico no final
+
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 7)
