1c87fcbac57ff178135497705e2e10d9ab9bf7e6
Wyllian Nascimento <Wyllian.nascimento@gmail.com>
2022-01-22 15:42:30 -0300

    Update ex017.js

--- aula15/ex017.js
+++ aula15/ex017.js
@@ -1,5 +1,6 @@
 let vetor = [7, 9, 11, 2, 32, 15, 12, 30, 71, 41, 47, 5]
 vetor[12] = 1 
 vetor.push(8)
+vetor.sort()
 
-console.log(`Nosso vetor é o ${vetor}`)
+console.log(`Nosso vetor é o ${vetor.sort()}`)
