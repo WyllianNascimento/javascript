9ef5caba00ba60107fe8882141461321e41082eb
Wyllian Nascimento <Wyllian.nascimento@gmail.com>
2022-01-22 20:59:40 -0300

    Update ex017.js

--- aula15/ex017.js
+++ aula15/ex017.js
@@ -1,14 +1,14 @@
-let vetor = [7, 9, 11, 2, 32, 15, 12, 30, 71, 41, 47, 5]
-vetor[12] = 1 // adciona valor na posição 12 do vetor
+let vetor = [7, 9, 0, 2, 5, 8, 3]
+vetor[5] = 1 // adciona valor na posição 12 do vetor
 vetor.push(8) // adciona valor ao vetor na próxima posição disponível
 /*
 vetor.lengh
 
 mostra o número total de posições do vetor
 */
-/*
-vetor.sort()
 
-organiza os valores em ordem crescente
-*/
+vetor.sort() //organiza os valores em ordem crescente
+
+console.log(vetor)
 console.log(`O vetor tem ${vetor.length} posições`)
+console.log(`O primeiro valor do vetor é ${vetor[0]}`)
