94ea6784334e9417c77c7059c16ca33253e2649e
Wyllian Nascimento <Wyllian.nascimento@gmail.com>
2022-01-28 15:17:38 -0300

    Update javascript.js

--- exercicios/001/javascript.js
+++ exercicios/001/javascript.js
@@ -5,7 +5,10 @@ function carregar() {
     var hora = data.getHours()
     var minuto = data.getMinutes()
     if (minuto < 10) {
-        minuto += "0"
+        minuto = "0" + minuto
+    }
+    if (hora < 10) {
+        hora = "0" + hora
     }
     msg.innerHTML = `Agora são ${hora}:${minuto}`
 }
