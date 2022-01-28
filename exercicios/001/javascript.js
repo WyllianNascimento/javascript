936f978c948da37d4280920a2ff44fd898605d7d
Wyllian Nascimento <Wyllian.nascimento@gmail.com>
2022-01-28 15:12:44 -0300

    update

--- exercicios/001/javascript.js
+++ exercicios/001/javascript.js
@@ -1,1 +1,8 @@
-alert ("olá")
+function carregar() {
+    var msg = document.querySelector('.msg')
+    var img = document.querySelector('.imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    var minuto = data.getMinutes()
+    msg.innerHTML = `Agora são ${hora}:${minuto}`
+}
