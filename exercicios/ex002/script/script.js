17bb1d20a2f00a7badbf4a37fbf4ccceca90b883
Wyllian Nascimento <Wyllian.nascimento@gmail.com>
2022-01-29 13:52:26 -0300

    Update script.js

--- exercicios/ex002/script/script.js
+++ exercicios/ex002/script/script.js
@@ -3,4 +3,10 @@ function verificar() {
     var ano = data.getFullYear()
     var fano = document.querySelector('#txtano')
     var res = document.querySelector('#res')
+
+    if(fano.value.length == 0 || fano.value > ano) {
+        alert ('[ERRO] Verifique os dados e tente novamente')
+    } else {
+        alert ('OK')
+    }
 }
