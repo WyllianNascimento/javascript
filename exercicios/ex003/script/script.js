358a8f5de0872c96eda4468b761e4775f43c0407
Wyllian Nascimento <Wyllian.nascimento@gmail.com>
2022-01-31 14:55:52 -0300

    Update script.js

--- exercicios/ex003/script/script.js
+++ exercicios/ex003/script/script.js
@@ -2,4 +2,8 @@ function contar() {
     let inicio = document.querySelector('#txti')
     let fim = document.querySelector('#txtf')
     let passo = document.querySelector('#txtp')
+
+    if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        
+    }
 }
