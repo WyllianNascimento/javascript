8235196bc6da0dc891daa606efdb069532b0a487
Wyllian Nascimento <Wyllian.nascimento@gmail.com>
2022-02-01 14:20:13 -0300

    Update script.js

--- exercicios/ex004/script/script.js
+++ exercicios/ex004/script/script.js
@@ -1,4 +1,10 @@
-function tabuada () {
+function tabuada() {
     let num = document.querySelector('#txtn')
     let tab = document.querySelector('#seltab')
+
+    if(num.value.length == 0) {
+        let n = Number(num.value)
+    } else {
+        alert('Por favor, digite um número!')
+    }
 }
