034e298b94e121bfcc69be0aa9f4b6f30310ecb3
Wyllian Nascimento <Wyllian.nascimento@gmail.com>
2022-02-02 15:33:34 -0300

    Update script.js

---
+++ exercicios/ex005/script/script.js
@@ -0,0 +1,16 @@
+let num = document.querySelector('#fnum')
+let lista = document.querySelector('#flista')
+let res = document.querySelector('#res')
+let valores = []
+
+function isNumero(n) {}
+
+function inLista(n) {}
+
+function adicionar() {
+    if(isNumero(num.value) && !inLista(num.value)) {
+
+    } else {
+        alert('Valor inválido ou já encontrado na lista.')
+    }
+}
